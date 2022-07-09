be58d611b21b702f6abfeabc7887377d3e60e227
Maria Serykh <marie.serykh@gmail.com>
2022-07-10 00:17:04 +0200

    fix: fix `userResolver`

--- app/modules/users/resolvers/user.resolver.ts
+++ app/modules/users/resolvers/user.resolver.ts
@@ -1,9 +1,5 @@
 export const userResolver = {
   Query: {
-    users: async (_: any, __: any, { dataSources }: any) => {
-      const res = await dataSources.tracksSource.getItems();
-      return res;
-    },
     user: async (_: any, id: string, { dataSources }: any) => {
       const res = await dataSources.tracksSource.getTtem(id);
       return res;
