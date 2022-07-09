ff3566fda6fa2bf54cd6f7ff1b0ab3ee9daa6f32
Maria Serykh <marie.serykh@gmail.com>
2022-07-10 00:16:33 +0200

    feat: add mutation

--- app/modules/albums/resolvers/album.resolver.ts
+++ app/modules/albums/resolvers/album.resolver.ts
@@ -1,12 +1,37 @@
+import { ApolloError } from 'apollo-server';
+import { NewAlbumInput } from '../album.types';
+
 export const albumResolver = {
   Query: {
-    albums: async (_: any, __: any, { dataSources }: any) => {
+    albums: async (
+      _: any,
+      { limit, offset }: { limit: number; offset: number },
+      { dataSources }: any,
+    ) => {
       const res = await dataSources.albumsSource.getItems();
       return res;
     },
-    album: async (_: any, id: string, { dataSources }: any) => {
-      const res = await dataSources.albumsSource.getTtem(id);
+    album: async (_: any, { id }: { id: string }, { dataSources }: any) => {
+      const res = await dataSources.albumsSource.getItem(id);
       return res;
+    },
+  },
+
+  Mutation: {
+    createAlbum: async (_: any, { input }: { input: NewAlbumInput }, { dataSources }: any) => {
+      try {
+        const newAlbumInput = { ...input };
+        const res = await dataSources.albumsSource.createItem(newAlbumInput);
+        return res;
+      } catch (err) {
+        const error = err as ApolloError;
+        return {
+          code: error.extensions.response.code,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
     },
   },
 
