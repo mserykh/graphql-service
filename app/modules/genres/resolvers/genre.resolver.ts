2f76086a6dc52e4732fed8448d0ead6498463f9d
Maria Serykh <marie.serykh@gmail.com>
2022-07-10 12:59:56 +0200

    feat: implement mutations and queries for genres

--- app/modules/genres/resolvers/genre.resolver.ts
+++ app/modules/genres/resolvers/genre.resolver.ts
@@ -1,12 +1,71 @@
+import { ApolloError } from 'apollo-server';
+import { Genre } from 'types';
+import { GenreInput, NewGenreInput } from '../genre.types';
+
 export const genreResolver = {
   Query: {
-    genres: async (_: any, __: any, { dataSources }: any) => {
-      const res = await dataSources.genresSource.getItems();
+    genres: async (
+      _: any,
+      { limit, offset }: { limit: number; offset: number },
+      { dataSources }: any,
+    ) => {
+      const res = await dataSources.genresSource.getItems(limit, offset);
       return res;
     },
-    genre: async (_: any, id: string, { dataSources }: any) => {
+    genre: async (_: any, { id }: { id: string }, { dataSources }: any) => {
       const res = await dataSources.genresSource.getItem(id);
       return res;
+    },
+  },
+
+  Mutation: {
+    createGenre: async (_: null, { input }: { input: NewGenreInput }, { dataSources }: any) => {
+      try {
+        const res = await dataSources.genresSource.createItem(input);
+        return res;
+      } catch (err) {
+        const error = err as ApolloError;
+        return {
+          code: error.extensions.response.code,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+
+    updateGenre: async (
+      _: null,
+      { id, input }: { id: string; input: GenreInput },
+      { dataSources }: any,
+    ) => {
+      try {
+        const res = await dataSources.genresSource.updateItem(id, input);
+        return res;
+      } catch (err) {
+        const error = err as ApolloError;
+        return {
+          code: error.extensions.response.code,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+
+    deleteGenre: async (_: null, { id }: { id: string }, { dataSources }: any) => {
+      try {
+        const res = await dataSources.genresSource.deleteItem(id);
+        return res;
+      } catch (err) {
+        const error = err as ApolloError;
+        return {
+          code: error.extensions.response.code,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
     },
   },
 
