94c576ffc01358365730f6fc90bc511b446c7aa3
Maria Serykh <marie.serykh@gmail.com>
2022-07-10 12:46:53 +0200

    feat: add token for requests, implement updating, deleting and getting items by id methods

--- app/DataSource.ts
+++ app/DataSource.ts
@@ -1,10 +1,14 @@
-import { RESTDataSource } from 'apollo-datasource-rest';
+import { RequestOptions, RESTDataSource } from 'apollo-datasource-rest';
 import { ApolloError } from 'apollo-server';
 
 export class DataSource extends RESTDataSource {
   constructor(url: string) {
     super();
     this.baseURL = url;
+  }
+
+  willSendRequest(request: RequestOptions) {
+    request.headers.set('Authorization', `Bearer ${this.context.token}`);
   }
 
   async getItems(limit = 5, offset = 0) {
@@ -39,6 +43,21 @@ export class DataSource extends RESTDataSource {
 
   async createItem(input: any) {
     const res = await this.post('', input);
-    return { ...res, id: res._id };
+    return res;
+  }
+
+  async updateItem(id: string, input: any) {
+    const res = await this.put(`/${id}`, input);
+    return res;
+  }
+
+  async deleteItem(id: string) {
+    const res = await this.delete(`/${id}`);
+    return res;
+  }
+
+  async getItemsById(ids: string[]) {
+    const res = await Promise.allSettled(ids.map((id) => this.getItem(id)));
+    return res;
   }
 }
